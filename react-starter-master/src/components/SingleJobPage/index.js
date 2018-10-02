0e2d6934e382e75a97289d7cb1ea03152be7ca5f
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-02 16:49:54 +0200

    Added reset.css

--- react-starter-master/src/components/SingleJobPage/index.js
+++ react-starter-master/src/components/SingleJobPage/index.js
@@ -9,7 +9,7 @@ class SingleJob extends React.Component {
           <a href="#">Back to List</a>
           <h1>Job Name</h1>
           <p>Lorem ipsum description</p>
-          <img src="#" />
+          <img src="#" alt="" />
           <div className="single-job-button-container">
             <button>Apply</button>
             <button>Share</button>
@@ -19,9 +19,9 @@ class SingleJob extends React.Component {
           </div>
           <h2>Some Colleagues</h2>
           <div className="single-job-image-container">
-            <img src="#" />
-            <img src="#" />
-            <img src="#" />
+            <img src="#" alt="" />
+            <img src="#" alt="" />
+            <img src="#" alt="" />
           </div>
           <a href="#">Back to List</a>
         </div>
