c76e9ef5a0614ea2540d93248b25162b24a839c0
Jeanette <aejlindberg@gmail.com>
2018-10-09 17:17:50 +0200

    landing page layout

--- react-starter-master/src/components/LandingPage/index.js
+++ react-starter-master/src/components/LandingPage/index.js
@@ -33,18 +33,26 @@ class LandingPage extends React.Component {
             <h1>Join us</h1>
             <p>lkjdlfkjlskdjf</p>
           </div>
-          <div className="job-suggestions-container">
-            <div className="job-suggestion">
-              <h3>{this.state.jobs[1].title}</h3>
-              <p>Description</p>
-            </div>
-            <div className="job-suggestion">
-              <h3>heading</h3>
-              <p>Description</p>
-            </div>
-            <div className="job-suggestion">
-              <h3>Heading</h3>
-            </div>
+        </div>
+        <div className="job-suggestions-container">
+          <div className="job-suggestion">
+            <h3>Heading</h3>
+            <p>Description</p>
+          </div>
+          <div className="job-suggestion">
+            <h3>Heading</h3>
+            <p>Description</p>
+          </div>
+          <div className="job-suggestion">
+            <h3>Heading</h3>
+            <p>Description</p>
+          </div>
+        </div>
+        <div className="job-openings-container">
+          <img src="./assets/images/comprend-hero-1920x800.jpg" alt="comprend" />
+          <div className="job-openings-info">
+            <h2>We are looking for you</h2>
+            <button className="button-job-openings">Job openings</button>
           </div>
         </div>
       </div>
