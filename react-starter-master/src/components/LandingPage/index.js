873687933e11f754df67c3d5122843be22f1c8cb
Jeanette <aejlindberg@gmail.com>
2018-10-09 16:56:43 +0200

    landing page first draft

--- react-starter-master/src/components/LandingPage/index.js
+++ react-starter-master/src/components/LandingPage/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./style.css";
+import React from "react"
+import "./style.css"
 
 class LandingPage extends React.Component {
   state = {
@@ -27,7 +27,26 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        LANDING PAGE!
+        <div className="hero-container">
+          <img src="./assets/images/comprend-hero2.webp" alt="comprend" />
+          <div className="hero-info-container">
+            <h1>Join us</h1>
+            <p>lkjdlfkjlskdjf</p>
+          </div>
+          <div className="job-suggestions-container">
+            <div className="job-suggestion">
+              <h3>{this.state.jobs[1].title}</h3>
+              <p>Description</p>
+            </div>
+            <div className="job-suggestion">
+              <h3>heading</h3>
+              <p>Description</p>
+            </div>
+            <div className="job-suggestion">
+              <h3>Heading</h3>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
