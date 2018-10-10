4285b7dd50eeca5328900aec9f9c892304c21732
Jeanette <aejlindberg@gmail.com>
2018-10-10 12:18:21 +0200

    Header

--- react-starter-master/src/components/app.js
+++ react-starter-master/src/components/app.js
@@ -3,12 +3,14 @@ import SingleJob from "./SingleJobPage"
 import JobListing from "./JobListing"
 import LandingPage from "./LandingPage"
 import EmployeePage from "./EmployeePage"
+import Header from "./HeaderComponent"
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
+        <Header />
         <LandingPage />
         {/* <EmployeePage /> */}
         {/* <JobListing /> */}
